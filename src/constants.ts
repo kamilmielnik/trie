fd5c189b7a3750a306a277d86673e10bac447a33
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2020-11-13 00:13:58 +0700

    Add JSDocs for constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,14 @@
+/**
+ * Represents end of a node in JSON (serialized format).
+ */
 export const CLOSE_PARENS = ')';
+
+/**
+ * Represents start of a node in JSON (serialized format).
+ */
 export const OPEN_PARENS = '(';
+
+/**
+ * Separates nodes in JSON (serialized format).
+ */
 export const SEPARATOR = ',';
