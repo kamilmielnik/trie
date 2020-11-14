6e75fb457786f71bdf7e5e417910cc83a62a4fb3
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2020-11-14 21:08:05 +0700

    Make traverse tests pass

--- src/lib/traverse.ts
+++ src/lib/traverse.ts
@@ -3,48 +3,66 @@ import { Node } from '../types';
 type Callback = (parameters: { node: Node; prefix: string }) => boolean | void;
 
 interface State {
-  node: Node;
   keyIndex: number;
+  node: Node;
 }
 
-const traverse = (node: Node, prefix: string, callback: Callback): boolean => {
+const sortKeys = (keys: string[]): string[] =>
+  keys.sort((key1, key2) => {
+    if (key1 === 'wordEnd') {
+      return -1;
+    }
+
+    if (key2 === 'wordEnd') {
+      return 1;
+    }
+
+    return key1.localeCompare(key2);
+  });
+
+const traverse = (node: Node, prefix: string, callback: Callback): void => {
   const stack: State[] = [];
-  let currentNode: Node | undefined = node;
   let currentKeyIndex = 0;
+  let currentNode: Node | undefined = node;
   let currentPrefix = prefix;
 
   while (stack.length > 0 || currentNode) {
-    if (currentNode) {
-      const keys = Object.keys(currentNode);
-      const key = keys[currentKeyIndex];
+    while (currentNode) {
+      const keys = sortKeys(Object.keys(currentNode));
 
       if (currentKeyIndex >= keys.length) {
         currentNode = undefined;
-      } else if (key === 'wordEnd') {
-        const shouldStop = callback({ node: currentNode, prefix: currentPrefix });
-
-        if (shouldStop) {
-          return true;
-        }
-
-        ++currentKeyIndex;
       } else {
-        stack.push({ node: currentNode, keyIndex: currentKeyIndex });
+        const key = keys[currentKeyIndex];
 
-        currentNode = currentNode[key] as Node | undefined;
-        currentKeyIndex = 0;
-        currentPrefix += key;
+        if (key === 'wordEnd') {
+          ++currentKeyIndex;
+        } else {
+          stack.push({ keyIndex: currentKeyIndex, node: currentNode });
+
+          currentKeyIndex = 0;
+          currentNode = currentNode[key] as Node | undefined;
+          currentPrefix += key;
+
+          if (currentNode) {
+            const shouldStop = callback({ node: currentNode, prefix: currentPrefix });
+
+            if (shouldStop) {
+              return;
+            }
+          }
+        }
       }
-    } else {
+    }
+
+    if (stack.length > 0) {
       const state = stack.pop() as State;
 
-      currentNode = state.node;
       currentKeyIndex = state.keyIndex + 1;
+      currentNode = state.node;
       currentPrefix = currentPrefix.slice(0, -1);
     }
   }
-
-  return false;
 };
 
 export default traverse;
