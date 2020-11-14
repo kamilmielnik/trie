bbb60891ff4a2db83598ba8340d4754becb3d99e
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2020-11-14 22:37:18 +0700

    Update test name

--- src/lib/nodeKeyComparator.test.ts
+++ src/lib/nodeKeyComparator.test.ts
@@ -1,12 +1,14 @@
 import nodeKeyComparator from './nodeKeyComparator';
 
-describe('Trie', () => {
-  it('Has all the words', () => {
-    expect(['a', 'wordEnd', 'z'].sort(nodeKeyComparator)).toEqual(['wordEnd', 'a', 'z']);
-    expect(['a', 'z', 'wordEnd'].sort(nodeKeyComparator)).toEqual(['wordEnd', 'a', 'z']);
-    expect(['z', 'wordEnd', 'a'].sort(nodeKeyComparator)).toEqual(['wordEnd', 'a', 'z']);
-    expect(['z', 'a', 'wordEnd'].sort(nodeKeyComparator)).toEqual(['wordEnd', 'a', 'z']);
-    expect(['wordEnd', 'z', 'a'].sort(nodeKeyComparator)).toEqual(['wordEnd', 'a', 'z']);
-    expect(['wordEnd', 'a', 'z'].sort(nodeKeyComparator)).toEqual(['wordEnd', 'a', 'z']);
+describe('nodeKeyComparator', () => {
+  it('Gives same result for all permutations when used for sorting', () => {
+    const sortedNodeKeys = ['wordEnd', 'a', 'z'];
+
+    expect(['a', 'wordEnd', 'z'].sort(nodeKeyComparator)).toEqual(sortedNodeKeys);
+    expect(['a', 'z', 'wordEnd'].sort(nodeKeyComparator)).toEqual(sortedNodeKeys);
+    expect(['z', 'wordEnd', 'a'].sort(nodeKeyComparator)).toEqual(sortedNodeKeys);
+    expect(['z', 'a', 'wordEnd'].sort(nodeKeyComparator)).toEqual(sortedNodeKeys);
+    expect(['wordEnd', 'z', 'a'].sort(nodeKeyComparator)).toEqual(sortedNodeKeys);
+    expect(['wordEnd', 'a', 'z'].sort(nodeKeyComparator)).toEqual(sortedNodeKeys);
   });
 });
