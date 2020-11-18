c81988081793f436d67dfa1a6b783c38f50d6e09
Kamil Mielnik <kamil.adam.mielnik@gmail.com>
2020-11-18 22:58:54 +0700

    Add JSDocs to 'nodeKeyComparator'

--- src/lib/nodeKeyComparator.ts
+++ src/lib/nodeKeyComparator.ts
@@ -1,3 +1,11 @@
+/**
+ * Comparator that allows sorting {@link Node} keys alphabetically
+ * with the exception of "wordEnd" which should always come first.
+ *
+ * @param key1 - First key to compare.
+ * @param key2 - Second key to compare.
+ * @returns -1 if key1 should precede key2, 0 if they're the same, 1 if key2 should precede key1.
+ */
 const nodeKeyComparator = (key1: string, key2: string): number => {
   if (key1 === 'wordEnd') {
     return -1;
